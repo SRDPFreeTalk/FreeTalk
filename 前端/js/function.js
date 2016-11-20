0da38714d9dc1cb9d43cbef6b578173b23d5c2b8
zjkzjk <179226997@qq.com>
2016-11-20 22:57:46 +0800

    notice页面js编辑完成

--- 前端/js/function.js
+++ 前端/js/function.js
@@ -6,33 +6,39 @@ $(document).ready(function(){
         $(".menu").slideToggle();
         $(".logo_onClick").show();
     });
+
     $(document).click(function(){
         $(".menu").slideUp();
         $(".logo_onClick").hide();
     });
+
     $(".menu,.showMenu").click(function(event){
         event.stopPropagation();
 
     });
+
     $(".thumbs_up").click(function(){
         $(this).toggleClass('thumbs_bg1').toggleClass('thumbs_bg2');
     });
+
     $(".chat").click(function(){
         $(this).toggleClass('chat_bg1').toggleClass('chat_bg2');
 
     });
-    $(".input_main textarea").bind("keydown keyup",function(){
-        $(this).autosize();
-    }).show().autosize();
 
-    $.fn.autosize = function(){
-        $(this).height('0px');
-        var setheight = $(this).get(0).scrollHeight;
-        if($(this).attr("_height") != setheight)
-            $(this).height(setheight+"px").attr("_height",setheight);
-        else
-            $(this).height($(this).attr("_height")+"px");
-    }
+    $(".notice_head .delete_box").click(function(){
+        $(".move_box").animate({left:'20px'});
+        $(".checkbox").animate({left:'15px'});
+        $(".none").show();
+
+    });
+    $(".back").click(function(){
+        $(".move_box").animate({left:'0'});
+        $(".checkbox").animate({left:'-20px'});
+        $(".none").hide();
+
+    });
+
 
 
 });
